c93dcf08c6cf30a6f47642e70f155cbca83c05dd
David Gasinski <dgasinski1@sheffield.ac.uk>
2025-07-15 14:01:54 +0100

    fixed bugs, added code to test on autoware without scenario runner

--- srunner/autoagents/autoware_agent.py
+++ srunner/autoagents/autoware_agent.py
@@ -8,12 +8,13 @@ import os
 import threading
 import signal
 import time
+import rclpy
 
 from dataclasses import dataclass
 
 import numpy
 
-import rclpy
+from rclpy.node import Node
 from std_msgs.msg import Header
 from geometry_msgs.msg import PoseStamped
 from autoware_vehicle_msgs.msg import Engage
@@ -34,19 +35,38 @@ class AutowareAgent():
     timestamp = None
     current_map_ = None
     published_plan = None
-    _global_plan_world_coord = None
+    _global_plan_world_coord = [
+        # {
+        #     "x": 85.4266,
+        #     "y": 1.66734,
+        #     "z": 0.0,
+        #     "yaw": 2.88377
+        # },
+        {
+            "x": 157.914,
+            "y": -9.2159,
+            "z": 0.0,
+            "yaw": 1.5694
+        },
+        {
+            "x": 92.3093,
+            "y": -198.465,
+            "z": 0.0,
+            "yaw": 1.57094
+        }
+    ]
    
-    def setup(self, path_to_conf_file: dict):
+    def setup(self, path_to_conf_file: dict = None):
         """Setup the Autoware Agent
 
         Args:
            path_to_conf_file (_dict_): Config file 
         """
         
-        self.node = RouteNode()
+        self.autoware_state = AutowareState()
+        self.node = RouteNode(self.autoware_state )
         self.logger = self.node.get_logger()
         
-        self.autoware_state = AutowareState()
         self.current_map_ = 'Town01'
         
         self.waypoint_iteration = 0
@@ -56,12 +76,16 @@ class AutowareAgent():
         if self.autoware_state.completed_route():
             self.autoware_state.reset_state()
         
+        
+
         if not (self.autoware_state.sent_route) and (self.autoware_state.within_goal()):
             # get waypoint
             waypoint = self._global_plan_world_coord[self.waypoint_iteration]
-            self.node.publish_goal(waypoint)
+            self.node.publish_goal_dict(waypoint) # change to non dict in scenario runner 
             
             self.waypoint_iteration += 1
+            
+            self.logger.info("testing")
             
             # check last waypoint hasn't been published
             if self.waypoint_iteration > (self.num_waypoints - 1):
@@ -91,16 +115,17 @@ class AutowareState:
     Used for deciding when to engage, send routes, etc...
 
     """
+    position: Point = None
+
     sent_route: bool = False
     sent_engage: bool = False
     
     motion_state: int = 0
     planning_state: int = 0
     
-    distance_to_goal: float = 0
+    distance_to_goal: float = -1
     min_dist: float = 0.5
     zero_dist: float = -1 # distance when completed goal ?
-    position: Point
         
     def within_goal(self):
         return True if (self.distance_to_goal >= self.min_dist or self.distance_to_goal == self.zero_dist) else False
@@ -114,9 +139,11 @@ class AutowareState:
     def reset_state(self) -> None:
        self.sent_route = False
        self.sent_engage = False
+       self.motion_state = 0
+       self.planning_state = 0
        self.distance_to_goal = -1
    
-class RouteNode(rclpy.Node):
+class RouteNode(Node):
     
     route = '/planning/mission_planning/goal'
     engage = '/autoware/engage'
@@ -157,14 +184,16 @@ class RouteNode(rclpy.Node):
         pose.header.frame_id = 'map'
         pose.header.stamp = self.time_stamp
         pose.pose.position.x = point['x']
-        pose.pose.position.y = -point['y']
+        pose.pose.position.y = point['y']
         pose.pose.position.z = point['z']
         quaternion = quaternion_from_euler(
-            0, 0, -math.radians(point['yaw']))
+            0, 0, point['yaw'])
         pose.pose.orientation.x = quaternion[0]
         pose.pose.orientation.y = quaternion[1]
         pose.pose.orientation.z = quaternion[2]
         pose.pose.orientation.w = quaternion[3]
+
+        self.logger.info(f"Point: {point}")
 
         self.route_pub_.publish(pose)
             
@@ -175,7 +204,7 @@ class RouteNode(rclpy.Node):
         pose.header.frame_id = 'map'
         pose.header.stamp = self.time_stamp
         pose.pose.position.x = point[0].location.x
-        pose.pose.position.y = -point[0].location.y
+        pose.pose.position.y = -point[0].location.y # may need to inverse, check performance first
         pose.pose.position.z = point[0].location.z
         quaternion = quaternion_from_euler(
             0, 0, -math.radians(point[0].rotation.yaw))
@@ -194,7 +223,7 @@ class RouteNode(rclpy.Node):
         self.state.sent_engage = state
 
 
-    def plan_state_cb(self, route_msg: RouteState) -> None:
+    def plan_state_cb_(self, route_msg: RouteState) -> None:
         """ Callback for ros2 topic
             /planning/mission_planning/state
         """
@@ -204,13 +233,13 @@ class RouteNode(rclpy.Node):
             self.logger.info(f"Planning State: {curr}")
             self.state.planning_state = curr
             
-    def traj_state_cb(self, traj_msg: Trajectory) -> None:
+    def traj_state_cb_(self, traj_msg: Trajectory) -> None:
         """ Callback for ros2 topic
             /planning/scenario_planning/trajectory'
         """
         self.state.position = traj_msg.points[-1].pose.position
     
-    def motion_state_cb(self, motion_msg: MotionState) -> None:
+    def motion_state_cb_(self, motion_msg: MotionState) -> None:
         """ Callback for ros2 topic
             /api/motion/state
         """
@@ -223,12 +252,24 @@ class RouteNode(rclpy.Node):
     def remaining_dist_cb_(self, time_distance_msg: MissionRemainingDistanceTime):
         self.state.distance_to_goal = time_distance_msg.remaining_distance
     
-if __name__ == '__main__':
+def tick_agent(agent, time_delay):
+    while True:
+        time.sleep(time_delay)
+        agent.run_step()
+
+def main(args=None):
+    rclpy.init(args=args)
     agent = AutowareAgent()
     agent.setup()
+
+    agent_thread = threading.Thread(target=tick_agent, args=(agent, 0.05,))
+    agent_thread.start()
     
-    iterations = 10
-    for i in range(0,iterations):
-        time.sleep(1)
-        agent.run_step(0,0)
-        
+    rclpy.spin(agent.node)
+
+    agent.node.destroy_node()
+    agent_thread.join()
+    rclpy.shutdown()
+
+if __name__ == '__main__': 
+    main()
