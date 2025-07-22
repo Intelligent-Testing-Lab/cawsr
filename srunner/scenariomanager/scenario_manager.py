7faf4a09b42b7c9752de7c94823fe1066554829b
David Gasinski <dgasinski1@sheffield.ac.uk>
2025-07-22 10:27:53 +0100

    more debug

--- srunner/scenariomanager/scenario_manager.py
+++ srunner/scenariomanager/scenario_manager.py
@@ -151,9 +151,10 @@ class ScenarioManager(object):
         Run next tick of scenario and the agent.
         If running synchornously, it also handles the ticking of the world.
         """
-
+        print("I got ticked")
         if self._timestamp_last_run < timestamp.elapsed_seconds and self._running:
             self._timestamp_last_run = timestamp.elapsed_seconds
+            print("I'm running and got ticked")
 
             self._watchdog.update()
 
@@ -176,6 +177,7 @@ class ScenarioManager(object):
                 sys.stdout.flush()
 
             if self.scenario_tree.status != py_trees.common.Status.RUNNING:
+                print("Set status to False")
                 self._running = False
 
         # if self._sync_mode and self._running and self._watchdog.get_status():
