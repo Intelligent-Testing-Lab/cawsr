7940597b9c292bc15183502b92c0971e36ad150b
David Gasinski <dgasinski1@sheffield.ac.uk>
2025-07-22 10:35:37 +0100

    fixed it finally; added some code back to test

--- srunner/autoagents/autoware_agent.py
+++ srunner/autoagents/autoware_agent.py
@@ -54,10 +54,10 @@ class AutowareAgent(AutonomousAgent):
         self.waypoints_world = self._global_plan_world_coord[:-1]
 
         # reinitialise localization
-        # self.autoware_node.request_localize()  # None uses GNSS
+        self.autoware_node.request_localize()  # None uses GNSS
 
         # clear route
-        # self.route_node.request_clear_route()
+        self.route_node.request_clear_route()
 
         self.agent_set_route = True
 
