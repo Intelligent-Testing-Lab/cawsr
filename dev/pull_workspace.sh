ea854b65353018aecd58b7530a9793622f465ac1
David Gasinski <dgasinski1@sheffield.ac.uk>
2026-03-15 19:09:10 +0000

    (feat) completed and tested development container

--- dev/pull_workspace.sh
+++ dev/pull_workspace.sh
@@ -2,3 +2,5 @@
 
 cd ..
 git clone https://github.com/Intelligent-Testing-Lab/cawsr_workspace.git
+
+find . -maxdepth 1 ! -name ".git" ! -name ".gitignore" ! -name "requirements.txt" ! -name "README.md" ! -name "docker-compose.yml" -exec mv {} /path/to/destination/ \;
