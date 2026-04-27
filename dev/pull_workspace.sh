4970a5826b472c51da88b3c808ff505f75f36b99
David Gasinski <dgasinski1@sheffield.ac.uk>
2026-04-27 18:31:15 +0100

    updated dev workspace script

--- dev/pull_workspace.sh
+++ dev/pull_workspace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # clone into current directory
 git clone https://github.com/Intelligent-Testing-Lab/cawsr_workspace.git
-find . -maxdepth 1 ! -name ".git" ! -name ".gitignore" ! -name "requirements.txt" ! -name "README.md" ! -name "docker-compose.yml" -exec mv {} ../ \;
+find ./cawsr_workspace -maxdepth 1 ! -name ".git" ! -name ".gitignore" ! -name "requirements.txt" ! -name "README.md" ! -name "docker-compose.yml" -exec mv {} ../ \;
