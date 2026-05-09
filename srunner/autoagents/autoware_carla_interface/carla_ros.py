74034330406c148532cb31a50fd1ec20b2582095
David Gasinski <dgasinski1@sheffield.ac.uk>
2026-05-09 02:10:10 +0100

    (bug) added annotations import to resolve imports only be static type checkers

--- srunner/autoagents/autoware_carla_interface/carla_ros.py
+++ srunner/autoagents/autoware_carla_interface/carla_ros.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.sr/bin/env python
 
+from __future__ import annotations
+
 import math
 import queue
 
